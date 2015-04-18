ab4272eb287384116251574b05724b7f4ae6a65c
ministeren <fcmikkelsen@gmail.com>
2015-04-18 11:43:23 +0200

    labdatabasef15 indlæst.

---
+++ 19_database/src/daointerfaces/ProduktBatchDAO.java
@@ -0,0 +1,12 @@
+package daointerfaces;
+
+import java.util.List;
+
+import dto.ProduktBatchDTO;
+
+public interface ProduktBatchDAO {
+	ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
+	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
+	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+}
--- 19_database/src/funtionality/Connector.java
+++
@@ -1,5 +0,0 @@
-package funtionality;
-
-public class Connector {
-
-}
