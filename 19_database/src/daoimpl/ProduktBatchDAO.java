491e8b51cd6bf672093b1a128222c50028965087
ministeren <ministeren@192.168.1.45>
2015-04-19 21:16:47 +0200

    produktbatchDAO implementeret.

--- 19_database/src/daoimpl/ProduktBatchDAO.java
+++ 19_database/src/daoimpl/ProduktBatchDAO.java
@@ -1,7 +1,11 @@
 package daoimpl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import connector.Connector;
 import daointerfaces.DALException;
 import daointerfaces.IProduktBatchDAO;
 import dto.ProduktBatchDTO;
@@ -10,28 +14,41 @@ public class ProduktBatchDAO implements IProduktBatchDAO {
 
 	@Override
 	public ProduktBatchDTO getProduktBatch(int pbId) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatch WHERE pb_id = " + pbId);
+		
+	    try {
+	    	if (!rs.first()) throw new DALException("Produktbatch " + pbId + " findes ikke");
+	    	return new ProduktBatchDTO (rs.getInt("pb_id"), rs.getInt("status"), rs.getInt("recept_id"));
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
 	}
 
 	@Override
 	public List<ProduktBatchDTO> getProduktBatchList() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<ProduktBatchDTO> list = new ArrayList<ProduktBatchDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatch");
+		try {
+			while (rs.next()) {
+				list.add(new ProduktBatchDTO(rs.getInt("pb_id"), rs.getInt("status"), rs.getInt("recept_id")));
+			}
+		} catch (SQLException e) { throw new DALException(e); }
+		return list;
 	}
 
 	@Override
-	public void createProduktBatch(ProduktBatchDTO produktbatch)
-			throws DALException {
-		// TODO Auto-generated method stub
-
+	public void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
+		Connector.doUpdate(
+				"INSERT INTO produktbatch (pb_id, status, recept_id) VALUES " +
+				"(" + produktbatch.getPbId() + ", " + produktbatch.getStatus() + ", " + produktbatch.getReceptId() + ")"
+			);
 	}
 
 	@Override
-	public void updateProduktBatch(ProduktBatchDTO produktbatch)
-			throws DALException {
-		// TODO Auto-generated method stub
-
+	public void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
+		Connector.doUpdate(
+				"UPDATE produktbatch SET  pb_id = " + produktbatch.getPbId() + ", status =  " + produktbatch.getStatus() + 
+				", recept_id = " + produktbatch.getReceptId() + " WHERE pb_id = " + produktbatch.getPbId()
+		);
 	}
 
 }
